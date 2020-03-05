1f762e1b36571c787043e86d3caa8af5f6d508e4
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-03-05 14:41:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,20 +1,16 @@
 def new_hash
-  # return an empty hash
   {}
 end
 
 def my_hash
   {:fruit => "Apple"}
-  # return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
   {:name => "Grace Hopper"}
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
   {:id => 3}
 end
 
